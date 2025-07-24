f319e68f35c7ad1a9b4419d51144ed243d28458d
Craig Menning <craig.menning@windriver.com>
2025-07-24 10:38:27 -0500

    Preparing for amplify deployment

--- javascript/vite.config.js
+++ javascript/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/asu-calculator/',
+  base: process.env.GITHUB_PAGES ? '/asu-calculator/' : '/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
